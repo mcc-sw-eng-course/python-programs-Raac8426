567ce90f78967c6077af4dcc2999955cb0f58297
Carlos Alvarez Romero <raac.carlos8426@gmail.com>
2019-03-21 08:22:49 -0600

    work in progress to make bot a better palyer

--- PythonPrograms/PythonPrograms/L8/Bot.py
+++ PythonPrograms/PythonPrograms/L8/Bot.py
@@ -10,3 +10,21 @@ class Bot(object):
         rand=random.randint(0,len(arr)-1)
         return arr[rand]['position']
     
+    def isBoardWin(self,possibleBoard):
+        if (possibleBoard[0] == possibleBoard[1] and possibleBoard[2] == possibleBoard[0] and possibleBoard[0]==self.player):
+            return True
+        if (possibleBoard[3] == possibleBoard[4] and possibleBoard[5] == possibleBoard[3] and possibleBoard[3]==self.player):
+            return True
+        if (possibleBoard[6] == possibleBoard[7] and possibleBoard[8] == possibleBoard[6] and possibleBoard[6]==self.player):
+            return True
+        if (possibleBoard[0] == possibleBoard[3] and possibleBoard[6] == possibleBoard[0] and possibleBoard[0]==self.player):
+            return True
+        if (possibleBoard[1] == possibleBoard[4] and possibleBoard[7] == possibleBoard[1] and possibleBoard[1]==self.player):
+            return True
+        if (possibleBoard[2] == possibleBoard[5] and possibleBoard[8] == possibleBoard[2] and possibleBoard[2]==self.player):
+            return True
+        if (possibleBoard[0] == possibleBoard[4] and possibleBoard[8] == possibleBoard[0] and possibleBoard[0]==self.player):
+            return True
+        if (possibleBoard[2] == possibleBoard[4] and possibleBoard[6] == possibleBoard[2] and possibleBoard[2]==self.player):
+            return True
+        return False                                                                                           
--- PythonPrograms/PythonPrograms/L8/Bot_test.py
+++ PythonPrograms/PythonPrograms/L8/Bot_test.py
@@ -4,10 +4,57 @@ import Board
 
 class FileSort_test(unittest.TestCase):
     def setUp(self):
-        self.Bot=Bot.Bot()
-        
+        self.player=2
+        self.bot=Bot.Bot(self.player)
+        self.board=Board.Board()
 
+    def test_isWinBoard_Line1(self):
+        self.board.doMovement(self.player,0)
+        self.board.doMovement(self.player,1)
+        self.board.doMovement(self.player,2)
+        self.assertEqual(True,self.bot.isBoardWin(self.board.getBoard()))
 
+    def test_isWinBoard_Line2(self):
+        self.board.doMovement(self.player,3)
+        self.board.doMovement(self.player,4)
+        self.board.doMovement(self.player,5)
+        self.assertEqual(True,self.bot.isBoardWin(self.board.getBoard()))
+
+    def test_isWinBoard_Line3(self):
+        self.board.doMovement(self.player,6)
+        self.board.doMovement(self.player,7)
+        self.board.doMovement(self.player,8)
+        self.assertEqual(True,self.bot.isBoardWin(self.board.getBoard()))
+
+    def test_isWinBoard_Line4(self):
+        self.board.doMovement(self.player,1)
+        self.board.doMovement(self.player,4)
+        self.board.doMovement(self.player,7)
+        self.assertEqual(True,self.bot.isBoardWin(self.board.getBoard()))
+
+    def test_isWinBoard_Line5(self):
+        self.board.doMovement(self.player,2)
+        self.board.doMovement(self.player,5)
+        self.board.doMovement(self.player,8)
+        self.assertEqual(True,self.bot.isBoardWin(self.board.getBoard()))
+
+    def test_isWinBoard_Line6(self):
+        self.board.doMovement(self.player,0)
+        self.board.doMovement(self.player,3)
+        self.board.doMovement(self.player,6)
+        self.assertEqual(True,self.bot.isBoardWin(self.board.getBoard()))
+
+    def test_isWinBoard_Line7(self):
+        self.board.doMovement(self.player,0)
+        self.board.doMovement(self.player,4)
+        self.board.doMovement(self.player,8)
+        self.assertEqual(True,self.bot.isBoardWin(self.board.getBoard()))
+
+    def test_isWinBoard_Line8(self):
+        self.board.doMovement(self.player,2)
+        self.board.doMovement(self.player,4)
+        self.board.doMovement(self.player,6)
+        self.assertEqual(True,self.bot.isBoardWin(self.board.getBoard()))
 
 if __name__ == '__main__':
     unittest.main()
